fa7a41390783d86612925e707761ff749c5a9a59
Taylor Heimbichner <theimbichner@email.arizona.edu>
2020-09-01 22:00:25 -0700

    Added sample unit tests and separated source from tests

---
+++ test/test_task.py
@@ -0,0 +1,42 @@
+def setup_function(function):
+    print('exec')
+    print(function)
+
+def test_init_table():
+    pass
+
+def test_init_task():
+    pass
+
+def test_init_generator():
+    pass
+
+def test_task_to_dict():
+    pass
+
+def test_task_from_dict():
+    pass
+
+def test_modify_task():
+    pass
+
+def test_modify_following_tasks():
+    pass
+
+def test_generator_to_dict():
+    pass
+
+def test_generator_from_dict():
+    pass
+
+def test_modify_generator():
+    pass
+
+def test_table_to_dict():
+    pass
+
+def test_table_from_dict():
+    pass
+
+def test_table_get_tasks():
+    pass
