b6df28447cb4fbee9733bc78a9a2e55b4a4419e6
Taylor Heimbichner <ta.heimbichner@gmail.com>
2020-12-21 16:34:13 -0700

    Update gitignore, add remove and add to SetList

--- src/main/java/io/github/theimbichner/task/collection/SetList.java
+++ src/main/java/io/github/theimbichner/task/collection/SetList.java
@@ -7,18 +7,45 @@ import io.vavr.collection.Vector;
 
 public class SetList<T> {
    private final HashSet<T> set;
-   private final Vector<T> list;
+   private HashSet<T> removed;
+   private Vector<T> list;
 
-   private SetList(HashSet<T> set, Vector<T> list) {
+   private SetList(HashSet<T> set, HashSet<T> removed, Vector<T> list) {
       this.set = set;
+      this.removed = removed;
       this.list = list;
    }
 
    public static <T> SetList<T> empty() {
-      return new SetList<>(HashSet.empty(), Vector.empty());
+      return new SetList<>(HashSet.empty(), HashSet.empty(), Vector.empty());
+   }
+
+   private void clean() {
+      list = list.removeAll(removed);
+      removed = HashSet.empty();
+   }
+
+   public SetList<T> add(T t) {
+      if (set.contains(t)) {
+         return this;
+      }
+      if (removed.contains(t)) {
+         clean();
+      }
+
+      return new SetList<>(set.add(t), removed, list.append(t));
+   }
+
+   public SetList<T> remove(T t) {
+      if (!set.contains(t)) {
+         return this;
+      }
+
+      return new SetList<>(set.remove(t), removed.add(t), list);
    }
 
    public List<T> asList() {
-       return list.asJava();
+      clean();
+      return list.asJava();
    }
 }
--- src/test/java/io/github/theimbichner/task/collection/SetListTests.java
+++ src/test/java/io/github/theimbichner/task/collection/SetListTests.java
@@ -2,13 +2,63 @@ package io.github.theimbichner.task.collection;
 
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.*;
 
 public class SetListTests {
+   SetList<String> setList;
+
+   @BeforeEach
+   void beforeEach() {
+      setList = SetList.empty();
+   }
+
    @Test
    void testEmpty() {
       assertThat(SetList.empty().asList()).isEqualTo(List.of());
+   }
+
+   @Test
+   void testAdd() {
+      setList = setList.add("alpha");
+      assertThat(setList.asList()).isEqualTo(List.of("alpha"));
+   }
+
+   @Test
+   void testAddDuplicate() {
+      setList = setList.add("alpha").add("alpha");
+      assertThat(setList.asList()).isEqualTo(List.of("alpha"));
+   }
+
+   @Test
+   void testAddDuplicateOrder() {
+      setList = setList.add("alpha").add("beta").add("gamma").add("beta");
+      assertThat(setList.asList()).isEqualTo(List.of("alpha", "beta", "gamma"));
+   }
+
+   @Test
+   void testAddRemoveAdd() {
+      setList = setList.add("alpha").add("beta").add("gamma").remove("beta").add("beta");
+      assertThat(setList.asList()).isEqualTo(List.of("alpha", "gamma", "beta"));
+   }
+
+   @Test
+   void testRemove() {
+      setList = setList.add("alpha").remove("alpha");
+      assertThat(setList.asList()).isEmpty();
+   }
+
+   @Test
+   void testRemoveAddRemove() {
+      setList = setList.add("alpha").add("beta").remove("alpha").add("alpha").remove("alpha");
+      assertThat(setList.asList()).isEqualTo(List.of("beta"));
+   }
+
+   @Test
+   void testRemoveAbsent() {
+      setList = setList.add("alpha").add("beta").remove("gamma");
+      assertThat(setList.asList()).isEqualTo(List.of("alpha", "beta"));
    }
 }
